58aa4e7562e682e99947b4af9175dd04a9d8bce0
The-Back-Up <gonzalodavidlopezdealcala@outlook.es>
2025-05-20 13:27:10 +0200

    Add files via upload
    Codigo tercera entrega

---
+++ saludo.ps1
@@ -0,0 +1,8 @@
+﻿#Ejercicio 1
+Clear-Host
+$nombre="Gonzalo David"
+$saludo="Buenos dias, buenas tardes y buenas noches"
+Write-Host $saludo "de parte de" $nombre
+#Añado esto al final porque si no el cmdlet se ejecuta solo un instante y no da tiempo
+# a revisar el resultado.
+Read-Host -Prompt "Presiona <Enter> para finalizar."
