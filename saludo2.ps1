11150b3de7a9903c09fa2d723ab9270cfb023d4f
The-Back-Up <gonzalodavidlopezdealcala@outlook.es>
2025-05-20 13:38:20 +0200

    Delete saludo2.ps1

--- saludo2.ps1
+++
@@ -1,7 +0,0 @@
-﻿#Ejercicio 2
-Clear-Host
-$nombre = Read-Host "Tu nombre: "
-$saludo = Read-Host "Tu saludo: "
-Clear-Host
-Write-Host $saludo "de parte de" $nombre
-Read-Host -Prompt "Presiona <Enter> para finalizar."
