c8d80b72695ca6ee33f4e38b37c0481fe158ab18
The-Back-Up <gonzalodavidlopezdealcala@outlook.es>
2025-05-20 13:38:41 +0200

    Add files via upload

---
+++ Tercera entrega/saludo2.ps1
@@ -0,0 +1,7 @@
+﻿#Ejercicio 2
+Clear-Host
+$nombre = Read-Host "Tu nombre: "
+$saludo = Read-Host "Tu saludo: "
+Clear-Host
+Write-Host $saludo "de parte de" $nombre
+Read-Host -Prompt "Presiona <Enter> para finalizar."
